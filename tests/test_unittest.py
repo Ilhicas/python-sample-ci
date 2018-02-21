b08f4af1e7996b6c4bf7aa1ff2882ec45d74b8d6
André Ilhicas dos Santos <andre.ilhicas@fiercely.pt>
2018-02-21 12:12:56 +0000

    Add failing tests to develop branch

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -2,13 +2,13 @@ import unittest, os
 
 class Example(unittest.TestCase):
     def test_sum(self):
-        self.assertEqual(1+1, 2, "SUM unittest failed")
+        self.assertEqual(1+1, 3, "SUM unittest failed")
 
     def test_subtract(self):
-        self.assertEqual(2-1, 1, "SUBTRACT unnittest failed")
+        self.assertEqual(2-1, 3, "SUBTRACT unnittest failed")
 
     def test_multiply(self):
-        self.assertEqual(1*2, 2, "MULTIPLY unnittest failed")
+        self.assertEqual(1*2, 3, "MULTIPLY unnittest failed")
 
     def test_division(self):
-        self.assertEqual(2/2, 1, "DIVISION unittest failed")
+        self.assertEqual(2/2, 3, "DIVISION unittest failed")
