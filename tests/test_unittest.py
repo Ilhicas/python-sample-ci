f944eb4b19caa17e61ffcf79e330a905eaac9169
swedge <scott.wedge@gmail.com>
2020-01-28 13:56:10 -0500

    Fix two typos

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -5,10 +5,10 @@ class Example(unittest.TestCase):
         self.assertEqual(1+1, 2, "SUM unittest failed")
 
     def test_subtract(self):
-        self.assertEqual(2-1, 1, "SUBTRACT unnittest failed")
+        self.assertEqual(2-1, 1, "SUBTRACT unittest failed")
 
     def test_multiply(self):
-        self.assertEqual(1*2, 2, "MULTIPLY unnittest failed")
+        self.assertEqual(1*2, 2, "MULTIPLY unittest failed")
 
     def test_division(self):
         self.assertEqual(2/2, 1, "DIVISION unittest failed")
